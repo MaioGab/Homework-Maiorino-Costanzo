463d60186848e2058e8187cad6d6880fd461ba2c
Dav2506 <dav.costanzo@stud.uniroma3.it>
2026-05-04 12:30:20 +0200

    Aggiunta delle classi StanzaMagicaProtected, StanzaProtected e StanzaBuia

---
+++ DiaDiaBase/src/it/uniroma3/diadia/ambienti/StanzaBuia.java
@@ -0,0 +1,20 @@
+package it.uniroma3.diadia.ambienti;
+
+public class StanzaBuia extends Stanza{
+	
+	private String attrezzoLuminoso;
+	
+	public StanzaBuia(String nome, String attrezzoLuminoso) {
+		super(nome);
+		this.attrezzoLuminoso=attrezzoLuminoso;
+	}
+	
+	@Override
+	public String getDescrizione() {
+		String buio= new String();
+		buio= "qui c'è un buio pesto";
+		if(!hasAttrezzo(attrezzoLuminoso))
+			return buio;
+		return super.getDescrizione();
+	}
+}
