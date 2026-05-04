fb4cca6507102b531ae8660f8cc17701f234d0ee
Utente <Utente@10.25.65.229>
2026-05-04 17:25:41 +0200

    Completamento Test StanzaBloccata

--- DiaDiaBase/test/test/StanzaBloccataTest.java
+++ DiaDiaBase/test/test/StanzaBloccataTest.java
@@ -1,5 +1,6 @@
 package test;
 import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.ambienti.StanzaBloccata;
 import it.uniroma3.diadia.attrezzi.Attrezzo;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,12 +8,42 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class StanzaBloccataTest {
+	private Stanza stanzaSud;
 	private Stanza stanzaAdiacente;
-	private Stanza stanzaCorrente;
+	private StanzaBloccata stanzaBloccata;
+	private Attrezzo osso;
+	private Attrezzo passepartout;
 	
 	@BeforeEach
 	void setup() {
-		stanzaCorrente = new Stanza("Atrio");
-		stanzaAdiacente = new Stanza(stanzaAdiacente);
+		stanzaBloccata = new StanzaBloccata("Biblioteca", "nord", "passepartout");
+		stanzaBloccata.impostaStanzaAdiacente("nord", stanzaAdiacente);
+		stanzaBloccata.impostaStanzaAdiacente("sud", stanzaSud);
+	}
+	
+	@Test
+	void testMancanzaAttrezzoAdatto() {
+		// Genero un elemento da inserire nella stanza bloccata (diverso da passepartout)
+		osso = new Attrezzo("osso", 1);
+		// Lo inserisco dentro la stanza
+		stanzaBloccata.addAttrezzo(osso);
+		// La descrizione di StanzaBloccata deve darmi il messaggio richiesto
+		assertEquals("Stanza bloccata nella direzione nord, prendi passepartout e posalo nella stanza ", stanzaBloccata.getDescrizione());
+	}
+
+	@Test
+	void testPresenzaAttrezzoAdatto() {
+		// Genero l'oggetto che mi serve per sbloccare la stanza
+		passepartout = new Attrezzo("passepartout", 1);
+		// Lo inserisco dentro la stanza
+		stanzaBloccata.addAttrezzo(passepartout);
+		// Verifico che effettivamente la porta sia aperta
+		assertEquals(stanzaAdiacente, stanzaBloccata.getStanzaAdiacente("nord"));
+	}
+	
+	@Test
+	void testDirezioneSenzaBlocchi() {
+		// Verifico che andando verso sud non ho blocchi
+		assertEquals(stanzaAdiacente, stanzaBloccata.getStanzaAdiacente("sud"));
 	}
 }
